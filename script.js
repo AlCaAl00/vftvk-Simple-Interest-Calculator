4c52d1d28b04933594f50ea52b8136eb5c036e97
AlCaAl00 <65672396+AlCaAl00@users.noreply.github.com>
2021-03-24 18:12:54 -0600

    Changes in index, css and script files

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//Dynamic Output for number scroller
+function rateUpdate(){
+    document.getElementById('displayRate').innerHTML = document.getElementById('rate').value;
 }
-        
+
+// Form result function
+function compute(){
+
+    var principle = document.getElementById ('principal').value;
+    var years = document.getElementById ('years').value;
+    var rate = document.getElementById ('rate').value;
+
+    var interest = parseFloat((principle*years*rate)/100)
+    var principal = parseFloat(principle)
+
+    if (this.principal.value == '' || principal <= 0) {
+        alert("Enter a positive number");
+        this.principal.focus();
+    }
+
+    else {
+        var result = document.getElementById('result');
+        result.innerHTML = "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "%</mark>.<br>" + "You will receive an amount of <mark>" + interest + "</mark>,<br> in the year <mark>" + (new Date().getFullYear() + years) + "</mark>";
+    }
+}
